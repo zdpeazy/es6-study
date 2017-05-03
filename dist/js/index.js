19f27b1a2441b991808158e1227991cbb7c200d9
eazdp <773937052@qq.com>
2017-05-03 17:01:59 +0800

    babel+es6基础环境搭建

---
+++ dist/js/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+input.map(function (item) {
+  return item + 1;
+});
+
+var Calc = function () {
+  function Calc() {
+    _classCallCheck(this, Calc);
+
+    console.log('Calc constructor');
+  }
+
+  _createClass(Calc, [{
+    key: 'add',
+    value: function add(a, b) {
+      return a + b;
+    }
+  }]);
+
+  return Calc;
+}();
+
+var c = new Calc();
+console.log(c.add(4, 5));
---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+input.map(item => item + 1);
+
+class Calc {
+    constructor() {
+      console.log('Calc constructor');
+    }
+    add(a, b) {
+      return a + b;
+    }
+  }
+
+  var c = new Calc();
+  console.log(c.add(4,5));
